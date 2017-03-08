34cfd4d277ff02f6840a9b394a491bb61d231e7b
rakshamangasuli <rasm16cs@cmrit.ac.in>
2017-03-08 14:40:06 +0530

    Add n complex

---
+++ week1/addncomplex.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+typedef struct{
+    int real;
+    int imaginary;
+}complex;
+complex num;
+complex findsum(int n)
+{
+	int i,real[n],imag[n];
+	for(i=0;i<n;i++){
+		int j=i+1;
+		printf("enter real and complex part of %d number:",j);
+		scanf("%d%d",&real[i],&imag[i]);
+		num.real+=real[i];
+		num.imaginary+=imag[i];
+	}
+	return num;
+}
+
+int main(){
+	int n;
+	printf("how many complex numbers do you want to add:");
+	scanf("%d",&n);
+	num=findsum(n);
+	printf("the sum is %d+i%d",num.real,num.imaginary);    
+    return 0;
+}
